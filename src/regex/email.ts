8780b344db6811625a5f819b7ccc634796cc9003
metalhead767 <metalhead767@gmail.com>
2019-06-27 19:01:09 +0600

    Add email regex

---
+++ src/regex/email.ts
@@ -0,0 +1,6 @@
+// taken from http://emailregex.com/
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // eslint-disable-line no-useless-escape
+
+export default function isValidEmail(str: string): boolean {
+  return emailRegex.test(str);
+}
